0e078ada6653960f4266b73b5c63207ddecc1bd4
Nick Oliver <nickoliver86@github.com>
2020-11-23 20:34:53 -0500

    Testing js injection

---
+++ inject.js
@@ -0,0 +1,10 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+  var s = document.createElement('script');
+  s.src = chrome.runtime.getURL('script.js');
+  s.onload = function() {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
+})();
---
+++ script.js
@@ -0,0 +1,5 @@
+(function() {
+  document.onkeydown = function(e) {
+    alert("hello");
+  };
+})();
